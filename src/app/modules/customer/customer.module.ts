d70a013bf267bfb272a176764d1019ea755e9350
Christian <christian.sticchi@hotmail.com>
2018-12-12 01:29:40 -0200

    criando tabela

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { FormComponent } from './components/form/form.component';
@@ -11,17 +12,20 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [FormComponent, ListComponent, AdminComponent],
   imports: [
     FormsModule,
     CommonModule,
+    HttpClientModule,
     CustomerRoutingModule,
     MatDividerModule,
     MatInputModule,
     MatButtonModule,
-    MatSelectModule
+    MatSelectModule,
+    MatTableModule
   ]
 })
 export class CustomerModule { }
--- src/app/shared/services/customer.service.ts
+++ src/app/shared/services/customer.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Customer } from '../models/customer';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   save(customer: Customer) {
     alert(JSON.stringify(customer));
+  }
+
+  findAll() {
+    const url = '';
+    return this.httpClient.get(url);
   }
 }
